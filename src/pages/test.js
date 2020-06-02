d012b94b90f540a294c385853ff68faaf6055d90
RafaelDavisH <rafaeldavish443@gmail.com>
2020-06-02 13:13:36 -0700

    pull updates

---
+++ src/pages/test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const test = () => {
+    return (
+        <div>
+            <p id="dev">This is a long The WORD Dev to be highlight</p>
+        </div>
+    );
+};
+
+export default test;
